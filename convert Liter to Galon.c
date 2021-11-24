3f8fd3555af5adc3f8e1af8bb5feee81f6252d65
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-24 10:11:01 +0600

    Add files via upload

---
+++ convert Liter to Galon.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float gr,li;
+    printf("Enter any galon number= ");
+    scanf("%f",&gr);
+    for(gr=1; gr<=1;gr++)
+    {
+        printf("%f= 3.785 X %f\n",li,3.785*gr);
+    }
+
+
+
+
+
+    return 0;
+}
